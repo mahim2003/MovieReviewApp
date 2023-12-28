96d78f494b9c3884ac10e6043d615a493ca7e2d2
mca265 <mahimchaudhry4@gmail.com>
2023-12-28 14:57:02 -0800

    Write dynamic query to look up movies by imdb id

--- movies/movies/src/main/java/dev/mahim/movies/MovieService.java
+++ movies/movies/src/main/java/dev/mahim/movies/MovieService.java
@@ -3,6 +3,8 @@ package dev.mahim.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class MovieService {
@@ -10,6 +12,10 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional <Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
     
 }
