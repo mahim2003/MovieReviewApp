c6d1cf4629ba60231d1b896709921907c096d429
mca265 <mahimchaudhry4@gmail.com>
2023-12-30 23:54:58 -0800

    Add CSS styling for the carousel and posters

--- movie-client/src/api/axiosConfig.js
+++ movie-client/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-   baseURL: 'https://2fb9-162-217-238-42.ngrok-free.app', 
+   baseURL: '', 
    headers: {"ngrok-skip-browser-warning":"true"}
 });
