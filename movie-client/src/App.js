b95c7b635d35a5b0997f175476ead74153d08787
mca265 <mahimchaudhry4@gmail.com>
2024-01-03 23:27:14 -0800

    Add and get reviews with HTTP requests

--- movie-client/src/App.js
+++ movie-client/src/App.js
@@ -7,18 +7,35 @@ import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
 import NotFound from './components/notfound/NotFound';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
 
   const getMovies = async () => {
     try {
       const response = await api.get("/api/v1/movies");
       setMovies(response.data);
+
     } catch (err) {
-      console.error(err);
+      console.log(err);
     }
-  };
+  }
+
+  const getMovieData = async (movieId) => {
+    try{
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviews);
+      
+    }
+    catch(error){
+      console.error(error);
+    }
+  }
 
   useEffect(() => {
     getMovies();
@@ -32,7 +49,7 @@ function App() {
           <Route path="/" element={<Layout/>}>
             <Route path="/" element={<Home movies={movies} />} ></Route>
             <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
-           
+            <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews}/>}></Route>
             <Route path="*" element = {<NotFound/>}></Route>
           </Route>
       </Routes>
